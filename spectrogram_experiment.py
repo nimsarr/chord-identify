d6b5df0351bc12618a6db0c9b801c7d1d9842567
Andreas Petrides <apetrides32@gmail.com>
2021-06-20 13:34:23 -0400

    added mp3 functionality and general os stuff

--- spectrogram_experiment.py
+++ spectrogram_experiment.py
@@ -1,13 +1,36 @@
+import os
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.io import wavfile
 from scipy.fft import fftshift
-import matplotlib.pyplot as plt
+from pydub import AudioSegment
 
 # This code contains a very simple example for how we want to eventually prepare the
 # training data for our NN
 
-sample_rate, samples = wavfile.read('karma_police.wav')
+mp3dir = './mp3_files'
+wavdir = './wav_files'
+
+mp3 = '.mp3'
+wav = '.wav'
+
+# https://www.geeksforgeeks.org/python-loop-through-files-of-certain-extensions/
+for mp3file in os.listdir(mp3dir):
+    if mp3file.endswith(mp3):
+        base_name = mp3file[0 : len(mp3file) - 4]
+        if not os.path.exists(base_name + wav):
+            sound = AudioSegment.from_mp3(mp3dir + '/' + mp3file)
+            sound.export(wavdir + '/' + base_name + wav, format="wav")
+            print("created " + base_name + wav)
+    else:
+        print("warning: non mp3 file found in mp3 directory")
+
+
+wavs = os.listdir(wavdir)
+
+print(wavdir + '/' + wavs[0])
+sample_rate, samples = wavfile.read(wavdir + '/' + wavs[0])
 
 start = 0
 spectrograms = []
